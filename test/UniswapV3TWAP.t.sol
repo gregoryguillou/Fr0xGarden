88937c259c4cfb2cf279d5417e0ded1e2513ccab
fr0xMaster <fr0xmaster@protonmail.com>
2023-05-22 10:31:13 +0200

    fix uniswapTwap test with arbitrum setting and add gas_reports to foundry config

--- test/UniswapV3TWAP.t.sol
+++ test/UniswapV3TWAP.t.sol
@@ -6,19 +6,19 @@ import "../src/UniswapV3TWAP.sol";
 
 contract UniswapV3TWAPTest is Test {
     UniswapV3TWAP public uniswapV3TWAP;
-    address FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
-    address TOKEN0 = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address TOKEN1 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984; // address of UNISWAP V3 FACTORY on Arbitrum network.
+    address TOKEN0 = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8; // USDC on Arbitrum network
+    address TOKEN1 = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1; // WETH on Arbitrum network
     uint24 UNISWAP_POOL_FEE = 3000;
-    uint256 mainnetFork;
+    uint256 arbitrumFork;
 
     function setUp() public {
-        string memory MAINNET_RPC_URL = vm.envString("MAINNET_RPC_URL");
-        mainnetFork = vm.createFork(MAINNET_RPC_URL);
+        string memory ARBITRUM_RPC_URL = vm.envString("ARBITRUM_RPC_URL");
+        arbitrumFork = vm.createFork(ARBITRUM_RPC_URL);
+        vm.selectFork(arbitrumFork);
     }
 
     function testEstimateAmountOut() public {
-        vm.selectFork(mainnetFork);
         uniswapV3TWAP = new UniswapV3TWAP(FACTORY, TOKEN0,TOKEN1,UNISWAP_POOL_FEE);
         uint256 price = uniswapV3TWAP.estimateAmountOut(TOKEN1, 10 ** 18, 4);
         emit log_named_uint("price : ", price);
